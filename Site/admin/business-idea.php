31dab3c794f8268d4d3932cfbf5e5f796533e433
nikom2532 <nikom2532@hotmail.com>
2013-10-07 04:17:40 +0700

    Backend: Business-idea List

--- Site/admin/business-idea.php
+++ Site/admin/business-idea.php
@@ -6,11 +6,11 @@ include($rootadminpath."include/header.php");
 //include($rootpath."include/opendb.php");
 include($rootadminpath."include/connect-to-database.php");
 //$_SESSION["userid"] = "";
-if ($_SESSION["userid"] == "") {
+if($_SESSION["userid"] == "") {
 	include ($rootadminpath . "include/login.php");
 	include ("include/footer.php");
-} 
-else {
+}
+else{
 	//check for Logout mode
 	if($_GET["mode"]=="logout"){
 		include($rootadminpath."include/module/logout_process.php");
@@ -24,10 +24,10 @@ else {
 			
 			<div class="page-full-width cf">
 				<ul id="tabs" class="left">
-					<li><a href="job.php">งาน</a></li>
-					<li><a href="business-idea.php" class="active-tab">ไอเดียธุรกิจ</a></li>
-					<li><a href="advertisement.php">โฆษณา</a></li>
-	                <li><a href="top-company.php">บริษัทชั้นนำ</a></li>
+					<li><a href="<?php echo $rootadminpath; ?>job.php">งาน</a></li>
+					<li><a href="<?php echo $rootadminpath; ?>business-idea.php" class="active-tab">ไอเดียธุรกิจ</a></li>
+					<li><a href="<?php echo $rootadminpath; ?>advertisement.php">โฆษณา</a></li>
+	                <li><a href="<?php echo $rootadminpath; ?>top-company.php">บริษัทชั้นนำ</a></li>
 				</ul> <!-- end tabs -->
 				<!-- company logo -->
 				
@@ -36,8 +36,6 @@ else {
 			
 		<!-- MAIN CONTENT -->
 		<div id="content">
-			
-	
 				<div class="content-module">
 					
 					<div class="content-module-main">
@@ -58,66 +56,45 @@ else {
 							</thead>
 	
 							<tfoot>
-							
-							
 							<tbody>
-	
-								<tr>
-									<td>1</td>
-									<td><a href="business-idea-detail.php" class="text-black">Adrian Purdila</a></td>
-	                                <td id="status"><img src="images/icons/message-boxes/confirmation.png" alt="active"></td>
-									<td id="action" class="center">
-	                                    <a href="edit-recommend-idea.php" class="table-actions-button text-blue">แก้ไข</a>
-	                                    <a href="#" class="table-actions-button text-red">ลบ</a>
-	                                </td>
-								</tr>
-	
-								<tr>
-									<td>2</td>
-									<td><a href="business-idea-detail.php" class="text-black">Adrian Purdila</a></td>
-	                                <td id="status"><img src="images/icons/message-boxes/error.png" alt="active"></td>
-									<td>
-	                                	<a href="edit-recommend-idea.php" class="table-actions-button text-blue">แก้ไข</a>
-	                                    <a href="#" class="table-actions-button text-red">ลบ</a>
-	                                </td>
-								</tr>
-	
-								<tr>
-									<td>3</td>
-									<td><a href="business-idea-detail.php" class="text-black">Adrian Purdila</a></td>
-	                                <td id="status"><img src="images/icons/message-boxes/confirmation.png" alt="active"></td>
-									<td>
-	                                	<a href="edit-recommend-idea.php" class="table-actions-button text-blue">แก้ไข</a>
-	                                    <a href="#" class="table-actions-button text-red">ลบ</a>
-	                                </td>
-								</tr>
-	
-								<tr>
-									<td>4</td>
-									<td><a href="business-idea-detail.php" class="text-black">Adrian Purdila</a></td>
-	                                <td id="status"><img src="images/icons/message-boxes/confirmation.png" alt="active"></td>
-									<td>
-	                                	<a href="edit-recommend-idea.php" class="table-actions-button text-blue">แก้ไข</a>
-	                                    <a href="#" class="table-actions-button text-red">ลบ</a>
-	                                </td>
-								</tr>
-	
-								<tr>
-									<td>5</td>
-									<td><a href="business-idea-detail.php" class="text-black">Adrian Purdila</a></td>
-	                                <td id="status"><img src="images/icons/message-boxes/error.png" alt="active"></td>
-									<td>
-	                                	<a href="edit-recommend-idea.php" class="table-actions-button text-blue">แก้ไข</a>
-	                                    <a href="#" class="table-actions-button text-red">ลบ</a>
-	                                </td>
-								</tr>
+<?php
+								$sql="
+									SELECT * 
+									FROM  `buildthedot_thaijobhd_job_idea` 
+								";
+								$result=@mysql_query($sql);
+								while($rs=@mysql_fetch_array($result)){
+?>
+									<tr>
+										<td><?php echo $rs["CompanyID"]; ?></td>
+										<td>
+											<a href="business-idea-detail.php" class="text-black"><?php echo $rs["MainIdea"]; ?></a>
+										</td>
+										<td id="status">
+											<img src="images/icons/message-boxes/<?php 
+												if($rs["IdeaRecomment"]==1) {
+													echo "confirmation.png";
+												}
+												elseif($rs["IdeaRecomment"]==0){
+													echo "error.png";
+												}
+											?>" alt="active" />
+										</td>
+										<td id="action" class="center">
+											<a href="edit-recommend-idea.php?CompanyID=<?php echo $rs["CompanyID"]; ?>" class="table-actions-button text-blue">แก้ไข</a>
+											<a href="#" class="table-actions-button text-red">ลบ</a>
+										</td>
+									</tr>
+<?php
+								}
+?>
 							</tbody>
 						</table>
 					</div> <!-- end content-module-main -->
 				</div> <!-- end content-module -->
 		</div> <!-- end content -->
-	<?php 
-		include($rootadminpath."include/footer.php");
+	<?php
+	include ($rootadminpath . "include/footer.php");
 	}
-}//end check user session
-?>
+	}//end check user session
+		?>
