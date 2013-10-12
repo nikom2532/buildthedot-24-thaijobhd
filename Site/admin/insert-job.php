421399c274e399b3f13e73630da0ff425be45ded
unknown <VAIO@.(none)>
2013-10-12 22:48:13 +0700

    5

--- Site/admin/insert-job.php
+++ Site/admin/insert-job.php
@@ -75,16 +75,16 @@
 		                          <div class="grid_2">
 		                             <h6 class="detail-title"> ตำแหน่ง</h6>
 		                          </div>
+		                          <div class="grid_8">
+		                          		<p><input type="text" id="name" name ="name" class="round" value=""/></p>
+		                          </div><br class="clear"/>
 		                          <div class="grid_2">
 		                                <h6 class="detail-title">สถานที่ปฏิบัติงาน</h6>   
 		                          </div>
 		                          <div class="grid_8">
 		                          		<p><textarea type="text" id="name" name ="name" class="round" value=""></textarea>				                                </p>
 		                          </div><br class="clear"/>
-		                          
-		                          <div class="grid_8">
-		                          		<p><textarea type="text" id="name" name ="name" class="round" value=""></textarea>                                </p>
-		                          </div><br class="clear"/>
+		                          <br class="clear"/>
 		                          <div class="grid_2">
 		                                <h6 class="detail-title">อัตรา</h6>   
 		                          </div>
