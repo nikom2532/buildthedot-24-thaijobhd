20aef0b68207e831ba8e8cb307f7c771dc338b23
nikom2532 <nikom2532@hotmail.com>
2013-10-17 22:31:46 +0700

    .

---
+++ Site/admin/include/connect-to-database-Server.php
@@ -0,0 +1,7 @@
+<?php
+       //$cn = @mysql_connect("localhost","root","");
+	   $cn = @mysql_connect("localhost","thaijobh","rZih229Ha7");
+	   //mysql_select_db("buildthe_thaijobhd",$cn);
+	   mysql_select_db("thaijobh_db",$cn);
+	   //mysql_query("SET NAMES tis620",$cn);
+?>
