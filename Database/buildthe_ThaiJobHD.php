5385f5566e9674f2316cde288fca65112f1b0881
nikom2532 <nikom2532@hotmail.com>
2013-10-25 01:05:50 +0700

    MySQL: add admin for user a@a.com

--- Database/buildthe_ThaiJobHD.php
+++ Database/buildthe_ThaiJobHD.php
@@ -10,10 +10,7 @@
 
 // `buildthe_ThaiJobHD`.`buildthedot_thaijobhd_ad`
 $buildthedot_thaijobhd_ad = array(
-  array('PictureID' => '1','AdPic' => '1','AdLink' => '1','AdType' => '','AdPosition' => ''),
-  array('PictureID' => '2','AdPic' => '1','AdLink' => '1','AdType' => '','AdPosition' => ''),
-  array('PictureID' => '3','AdPic' => '','AdLink' => 'weeerrr','AdType' => 'Side_Ads','AdPosition' => '2'),
-  array('PictureID' => '4','AdPic' => '','AdLink' => 'wert','AdType' => 'Side_Ads','AdPosition' => '3')
+  array('PictureID' => '11','AdPic' => 'ad_1_Content_Ads_1_1381658798_Add New Patient - Google Chrome_034.png','AdLink' => 'asdf','AdType' => 'Side_Ads','AdPosition' => '5')
 );
 
 );
@@ -22,12 +19,12 @@ $buildthedot_thaijobhd_ad = array(
 
 // `buildthe_ThaiJobHD`.`buildthedot_thaijobhd_job_idea`
 $buildthedot_thaijobhd_job_idea = array(
-  array('CompanyID' => '1','MainIdea' => 'Adrian Purdila test','Description1' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.','Description2' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.','Description3' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.','Pic1' => 'picture.png','Pic2' => 'picture.png','Pic3' => 'picture.png','IdeaRecomment' => '0','IdeaTime' => '2013-10-07 00:00:00','Status' => '1')
+  array('CompanyID' => '7','MainIdea' => 'aa','Description1' => 'aa','Description2' => 'aa','Description3' => 'aa','Pic1' => 'bi_1_pic1_1381674410_root@mingLaptop:-home-nikom2532_029.png','Pic2' => 'bi_1_pic2_1381674410_Screenshotfrom2012-09-2719:08:11.png','Pic3' => 'bi_1_pic3_1381674410_Screenshotfrom2013-02-0915:51:37.png','IdeaRecomment' => '1','IdeaTime' => '0000-00-00 00:00:00','Status' => '0')
 );
 
 );
 
 // `buildthe_ThaiJobHD`.`buildthedot_thaijobhd_user_account`
 $buildthedot_thaijobhd_user_account = array(
-  array('id' => '1','email' => 'a@a.com','password' => '77de54ccf56eb6f7dbf99e4d3be949ab6f9b0a55df8ac28564cb9f63a10be8af6ab3f7c2','firstname' => 'a','midname' => 'a','lastname' => 'a','job_id' => '1','job_status' => '1','admin_status' => '0','address' => '')
+  array('id' => '1','email' => 'a@a.com','password' => '77de54ccf56eb6f7dbf99e4d3be949ab6f9b0a55df8ac28564cb9f63a10be8af6ab3f7c2','firstname' => 'a','midname' => 'a','lastname' => 'a','job_id' => '1','job_status' => '1','admin_status' => '1','address' => '')
 );
