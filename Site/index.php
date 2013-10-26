e7af90334e6336dc4a850bc1a70ac71cad4c57ee
nikom2532 <nikom2532@hotmail.com>
2013-10-26 14:44:53 +0700

    Fix Front-End CSS

--- Site/index.php
+++ Site/index.php
@@ -1,12 +1,13 @@
 <?php
-include ("include/header.php");
+session_start();
+$rootpath = "./";
+$rootadminpath = "./admin/";
+include ($rootpath . "include/header.php");
+include ($rootadminpath . "include/connect-to-database.php");
+include ($rootpath . "include/top-menu.php");
+include ($rootpath . "include/search-bar.php");
 ?>
-<?php
-	include ("include/top-menu.php");
-?>
-<?php
-	include ("include/search-bar.php");
-?>
+<?php /* ?>
 <!--start banner -->
 <div id="banner-1">
 	<img src="admin/images/banner-2.png" width="144" height="143">
@@ -55,169 +56,176 @@ include ("include/header.php");
 	<img src="admin/images/banner-2.png" width="144" height="143">
 </div>
 <!--end banner side-right -->
-
+<?php */ ?>
 <div  id="slide-image" class="container_12">
 	<img src="images/slide-image.jpg" width="1000" height="280" alt="images">
 </div><!--end slide-image-->
 <div id="content" class="container_12">
-	<div id="head-title">
-		<h1>งานแนะนำ</h1>
+	<div class="grid_2 margin-left-5 margin-right-5">
+		adsf
 	</div>
-	<div class="grid_6" id="content-reccommend">
-		<h2 id="sub-title">Full Time <span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p class="border-none">
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<div id="banner">
-			<img src="images/banner-1.png" width="458" height="175" alt="Banner">
+	<div class="grid_8"><?php //Center Content ?>
+		<div id="head-title">
+			<h1>งานแนะนำ</h1>
 		</div>
-	</div>
-
-	<div class="grid_6" id="content-reccommend">
-		<h2 id="sub-title">Part Time <span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<div id="banner">
-			<img src="images/banner-1.png" width="458" height="175" alt="Banner">
-		</div>
-	</div>
-
-	<br class="clear seperator"/>
-
-	<div id="head-title">
-		<h1>งานใหม่ล่าสุด </h1>
-	</div>
-
-	<div class="grid_6" id="content-reccommend">
-		<h2 id="sub-title">Full time<span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-	</div>
-	<div class="grid_6" id="content-reccommend">
-		<h2 id="sub-title">Full time<span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-		<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-		</p>
-	</div>
-
-	<br class="clear seperator"/>
-
-	<div id="head-title">
-		<h1>บริษัทชั้นนำ</h1>
-	</div>
-	<div id="top-company">
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-	</div>
-	<div id="top-company">
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<div class="grid_2"><img src="images/banner-2.png" width="144" height="143">
-		</div>
-		<h2 class="right"><a href="#" class="button round black">ดูทั้งหมด</a></h2>
-	</div>
-
-	<br class="clear seperator"/>
-
-	<div id="head-title">
-		<h1>ไอเดียธุรกิจแนะนำ </h1>
-	</div>
-	<div id="recommend-idea">
-		<div class="grid_3">
-			<img src="images/banner-2.png" width="144" height="143">
-			<h3>Lorem Ipsum</h3>
+		<div class="grid_4 margin-left-5 margin-right-5" id="content-reccommend">
+			<h2 id="sub-title">Full Time <span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
 			<p>
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p class="border-none">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<div id="banner">
+				<img src="images/banner-1.png" width="100%" <?php // width="458" height="175" ?> alt="Banner">
+			</div>
+		</div>
+	
+		<div class="grid_4 margin-left-5 margin-right-5" id="content-reccommend">
+			<h2 id="sub-title">Part Time <span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<div id="banner">
+				<img src="images/banner-1.png" width="100%" <?php // width="458" height="175" ?> alt="Banner">
+			</div>
+		</div>
+	
+		<br class="clear seperator"/>
+	
+		<div id="head-title">
+			<h1>งานใหม่ล่าสุด </h1>
+		</div>
+	
+		<div class="grid_4 margin-left-5 margin-right-5" id="content-reccommend">
+			<h2 id="sub-title">Full time<span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 			</p>
 		</div>
-		<div class="grid_3">
-			<img src="images/banner-2.png" width="144" height="143">
-			<h3>Lorem Ipsum</h3>
+		<div class="grid_4 margin-left-5 margin-right-5" id="content-reccommend">
+			<h2 id="sub-title">Full time<span class="right"><a href="#" class="button round black">ดูทั้งหมด</a></span></h2>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
 			<p>
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+			</p>
+			<h6 id="headline">Lorem Ipsum is simply dummy text of the printing</h6>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 			</p>
 		</div>
-		<div class="grid_3">
-			<img src="images/banner-2.png" width="144" height="143">
-			<h3>Lorem Ipsum</h3>
-			<p>
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
-			</p>
+	
+		<br class="clear seperator"/>
+	
+		<div id="head-title">
+			<h1>บริษัทชั้นนำ</h1>
 		</div>
-		<div class="grid_3">
-			<img src="images/banner-2.png" width="144" height="143">
-			<h3>Lorem Ipsum</h3>
-			<p>
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
-			</p>
+		<div id="top-company">
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
 		</div>
-		<h2 class="right"><a href="#" class="button round black">ดูทั้งหมด</a></h2>
+		<div id="top-company">
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<div class="grid_2 margin-left-5 margin-right-5"><img src="images/banner-2.png" width="144" height="143">
+			</div>
+			<h2 class="right"><a href="#" class="button round black">ดูทั้งหมด</a></h2>
+		</div>
+	
+		<br class="clear seperator"/>
+	
+		<div id="head-title">
+			<h1>ไอเดียธุรกิจแนะนำ </h1>
+		</div>
+		<div id="recommend-idea">
+			<div class="grid_3">
+				<img src="images/banner-2.png" width="144" height="143">
+				<h3>Lorem Ipsum</h3>
+				<p>
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				</p>
+			</div>
+			<div class="grid_3">
+				<img src="images/banner-2.png" width="144" height="143">
+				<h3>Lorem Ipsum</h3>
+				<p>
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				</p>
+			</div>
+			<div class="grid_3">
+				<img src="images/banner-2.png" width="144" height="143">
+				<h3>Lorem Ipsum</h3>
+				<p>
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				</p>
+			</div>
+			<div class="grid_3">
+				<img src="images/banner-2.png" width="144" height="143">
+				<h3>Lorem Ipsum</h3>
+				<p>
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <span class="right" id="read-more"><a href="#">อ่านต่อ</a></span>
+				</p>
+			</div>
+			<h2 class="right"><a href="#" class="button round black">ดูทั้งหมด</a></h2>
+		</div>
+		<br class="clear seperator"/>
+	
+		<div class="grid_6">
+			<img src="images/banner-1.png" width="100%" <?php // width="458" height="175" ?> alt="Banner">
+		</div>
+		<div class="grid_6">
+			<img src="images/banner-1.png" width="100%" <?php // width="458" height="175" ?> alt="Banner">
+		</div>
+	</div><?php //End Center Content ?>
+	<div class="grid_2 margin-left-5 margin-right-5">
+		adsf
 	</div>
-	<br class="clear seperator"/>
-
-	<div class="grid_6">
-		<img src="images/banner-1.png" width="458" height="175">
-	</div>
-	<div class="grid_6">
-		<img src="images/banner-1.png" width="458" height="175">
-	</div>
-
 </div><!--end content -->
 <?php
 	include ("include/footer.php");
--- Site/login.php
+++ Site/login.php
@@ -1,10 +1,14 @@
-<?php include("include/header.php");?>
-<body>
+<?php
+session_start();
+$rootpath = "./";
+$rootadminpath = "./admin/";
+include ($rootpath . "include/header.php");
+include ($rootadminpath . "include/connect-to-database.php");
+?>
 <div id="wrapper">
 	<!-- HEADER -->
 	<div id="header" class="container_12">
-		
-		 <div id="login-intro" class="grid_3">
+		<div id="login-intro" class="grid_3">
 			<img src="images/logo.png" width="128" height="71">
 		</div> <!-- end full-width -->	
 
@@ -12,19 +16,19 @@
 		
 	<!-- MAIN CONTENT -->
 	<div id="content" class="container_12">
-		 <div id="content-login">
-        	<div id="head-title">
-    			<h1>เข้าสู่ระบบ</h1>
-    		</div> 
-            <form action="#" method="POST" id="login-form"  autocomplete="off">
+	 <div id="content-login">
+			<div id="head-title">
+				<h1>เข้าสู่ระบบ</h1>
+  		</div> 
+			<form action="#" method="POST" id="login-form"  autocomplete="off">
 				<p class="grid_2 prefix_3 uppercase">ชื่อผู้ใช้</p>
-           	 	<p class="grid_5"><input type="text" id="login-username" name ="login-username" class="round" /></p><br class="clear" />
+				<p class="grid_5"><input type="text" id="login-username" name ="login-username" class="round" /></p><br class="clear" />
 				<p class="grid_2 prefix_3 uppercase">รหัสผ่าน</p>
-           	 	<p class="grid_5"><input type="password" id="login-password" name ="login-password" class="round" /></p><br class="clear" />
-               
-                <p class="grid_2 prefix_5"><input type="submit" class="login-button blue round" value="เข้าสู่ระบบ"></p>
-    			<p id="forgot-pass" class="grid_2 prefix_5"><a href="forgot-password-username.php">ลืมรหัสผ่าน?</a></p>
-            </form>
+				<p class="grid_5"><input type="password" id="login-password" name ="login-password" class="round" /></p><br class="clear" />
+				
+				<p class="grid_2 prefix_5"><input type="submit" class="login-button blue round" value="เข้าสู่ระบบ"></p>
+				<p id="forgot-pass" class="grid_2 prefix_5"><a href="forgot-password-username.php">ลืมรหัสผ่าน?</a></p>
+			</form>
 		</div>
 	</div> <!-- end content -->
 	<!-- FOOTER -->
