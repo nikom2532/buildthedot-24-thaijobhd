ad3db9c554008c189ababe98d0a2fe00379bbc3b
unknown <VAIO@.(none)>
2013-10-27 19:47:46 +0700

    event when submit contract us

--- Site/contact-us.php
+++ Site/contact-us.php
@@ -4,40 +4,74 @@ include ("include/header.php");
 <?php
 	include ("include/top-menu.php");	
 ?>
+<script type="text/javascript">
 
+	function check_value_data()
+	{
+		var name = document.getElementById("name").value;
+		if(name == "" || name == null)
+		{
+			alert("Name is not valid");
+			return false;
+		}
+		
+		var mail = document.getElementById("mail").value;
+		if(mail == "" || mail == null)
+		{
+			alert("E-mail is not valid");
+			return false;
+		}
+		
+		var tel = document.getElementById("tel").value;
+		if(tel == "" || tel == null)
+		{
+			alert("Telephone number is not valid");
+			return false;
+		}
+		
+		var text = document.getElementById("text3").value;
+		if(text == "" || text == null)
+		{
+			alert("Text is not valid");
+			return false;
+		}
+	}
+
+</script>
+	
 <div id="content" class="container_12">
 	<div id="content-profile">
 		<div id="head-title">
 			<h1>ติดต่อเรา</h1>
 		</div>
 		<div class="grid_7">
-			<form id="contact-form" action="send-mail.php" name="send-email" method="post">
+			<form id="contact-form" action="send-mail.php" name="send-email" method="post" onsubmit="return check_value_data()">
 				<p class="grid_2">
 					ชื่อ
 				</p>
 				<p class="grid_4">
-					<input type="text" name ="name" class="round full-width" />
+					<input type="text" name ="name" id="name" class="round full-width" />
 				</p>
 				<br class="clear"/>
 				<p class="grid_2">
 					อีเมลล์
 				</p>
 				<p class="grid_4">
-					<input type="text"  name ="mail" class="round full-width" />
+					<input type="text"  name="mail" id="mail" class="round full-width" />
 				</p>
 				<br class="clear"/>
 				<p class="grid_2">
 					เบอร์โทรศัพท์
 				</p>
 				<p class="grid_4">
-					<input type="text" name ="tel" class="round full-width" />
+					<input type="text" name="tel" id="tel" class="round full-width" />
 				</p>
 				<br class="clear"/>
 				<p class="grid_2">
 					เรื่องที่ติดต่อ
 				</p>
 				<p class="grid_4">
-					<textarea type="text"  name ="text" class="round full-width" />
+					<textarea type="text"  name="text" id="text3" class="round full-width" />
 					</textarea>
 				</p>
 				<br class="clear"/>
