2ce42fcb29be9d64045ffa898ea00f833892256c
nikom2532 <nikom2532@hotmail.com>
2013-10-31 16:08:07 +0700

    .

--- Site/edit-profile.php
+++ Site/edit-profile.php
@@ -17,164 +17,142 @@ else{
 	";
 	$result_user = @mysql_query($sql_user);
 	if($rs_user = @mysql_fetch_array($result_user)){
-?>
-	
-<div id="wrapper">
-    <div id="header" class="container_12">
-        <div id="login-intro" class="grid_3">
-            <img src="images/logo.png" alt="ThaiJobHD" width="130" height="71">	
-        </div>
-        <div class="right">
-        	<ul id="my-profile">
-            	<li><a href="view-profile.php" class="text-blue">Wc Fone</a></li>
-                <li><a href="#" class="text-grey">Logout</a></li>
-            </ul>
-        </div>
-        <div id="top-nav" class="grid_8 prefix_1">
-            <ul>
-                <li><a href="index.php">หน้าหลัก<br/><span id="sub-memu">JobHD</span></a></li>
-                <li><a href="find-job.php">หางาน<br/><span id="sub-memu">สมัครงาน</span></a></li>
-                <li><a href="business-idea.php">ไอเดียธุรกิจ<br/><span id="sub-memu">สมัครงาน</span></a></li>
-                <li><a href="advertisement-rate.php">อัตราโฆษณา<br/><span id="sub-memu">ประกาศ</span></a></li>
-                <li><a href="condition.php">เงื่อนไข<br/><span id="sub-memu">ข้อตกลง</span></a></li>
-                <li><a href="contact-us.php">ติดต่อทีมงาน<br/><span id="sub-memu">ติดต่อเรา</span></a></li>
-            </ul>	
-        </div>
-     </div><!--end header -->       
-    <div id="content" class="container_12">
-    	<div id="content-profile">
-        	<div id="head-title">
-    			<h1>ฝากประวัติ <span class="text-blue">-  แก้ไขประวัติส่วนตัว</span></h1>
-    		</div> 
-            
-          	<h2><a href="#" class="button black round">เพิ่มรูป</a></h2>
-            <p class="grid_2">ขนาด 200*200 px</p><br class="clear"/>
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-           
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-             <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-             <p class="grid_4 prefix_2">
-            	 <label for="" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            		ผ่านการเกณฑ์ทหาร
-            	</label>
-				<label for="" class="alt-label"><input type="radio" id="test" name="test" />
-           			ศึกษาวิชาการ
-            	</label>         
-            </p><br class="clear"/>
-             <p class="grid_4 prefix_2">
-            	 <label for="" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            		ได้รับการยกเว้น
-            	</label>
-				<label for="" class="alt-label"><input type="radio" id="test" name="test" />
-           			อื่นๆ
-            	</label>         
-            </p><br class="clear"/>
-            <p class="grid_2">สถานะที่อยู่</p>							
-			<p class="grid_8">	
-            <label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            	บ้านส่วนตัว
-            </label>
-			<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-           		บ้านเช่า
-            </label>
-            <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-            	อาศัยบิดามารดา
-            </label>
-			</p><br class="clear"/>
-            
-            <p class="grid_2">สถานะ</p>							
-			<p class="grid_8">	
-            <label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            	โสด
-            </label>
-			<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-           		สมรส
-            </label>
-            <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-            	หย่า
-            </label>
-             <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-            	แยกกันอยู่
-            </label>
-			</p><br class="clear"/>
-             <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">Lorem Ipsum</p>
-            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-                     
-            <p class="grid_2">ชื่อบิดา</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            <p class="grid_2 prefix_2">อายุ<input type="text" id="name" name ="name" class="round input-small" /></p>
-            <p class="grid_2">อาชีพ<input type="text" id="name" name ="name" class="round input-small" /></p>
-            <p class="grid_4">
-            	<label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            	มีชีวิต
-            </label>
-			<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-           		ถึงแก่กรรม
-            </label>
-            </p><br class="clear"/>
-            
-            <p class="grid_2">ชื่อมารดา</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            <p class="grid_2 prefix_2">อายุ<input type="text" id="name" name ="name" class="round input-small" /></p>
-            <p class="grid_2">อาชีพ<input type="text" id="name" name ="name" class="round input-small" /></p>
-            <p class="grid_4">
-            	<label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
-            	มีชีวิต
-            </label>
-			<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
-           		ถึงแก่กรรม
-            </label>
-            </p><br class="clear"/>
-           
-            
-            <p class="grid_4">บุคคลอ้างอิงที่ติดต่อได้</p><br class="clear"/>
-            
-            <p class="grid_2">ชื่อ</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">ความสัมพันธ์</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">สถานที่ทำงาน</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">ตำแหน่ง</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-            
-            <p class="grid_2">เบอร์โทรศัพท์</p>
-            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
-                  
-           	<p class="grid_12 center"><a href="#" class="save-button blue round">บันทึก</a></p>
-            
-      	</div>	
-    </div><!--end content -->
+?>	
+		<div id="wrapper">
+	    <div id="content" class="container_12">
+	    	<div id="content-profile">
+	        	<div id="head-title">
+	    			<h1>ฝากประวัติ <span class="text-blue">-  แก้ไขประวัติส่วนตัว</span></h1>
+	    		</div> 
+	            
+	          	<h2><a href="#" class="button black round">เพิ่มรูป</a></h2>
+	            <p class="grid_2">ขนาด 200*200 px</p><br class="clear"/>
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	           
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	             <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear" />
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	             <p class="grid_4 prefix_2">
+	            	 <label for="" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            		ผ่านการเกณฑ์ทหาร
+	            	</label>
+					<label for="" class="alt-label"><input type="radio" id="test" name="test" />
+	           			ศึกษาวิชาการ
+	            	</label>         
+	            </p><br class="clear"/>
+	             <p class="grid_4 prefix_2">
+	            	 <label for="" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            		ได้รับการยกเว้น
+	            	</label>
+					<label for="" class="alt-label"><input type="radio" id="test" name="test" />
+	           			อื่นๆ
+	            	</label>         
+	            </p><br class="clear"/>
+	            <p class="grid_2">สถานะที่อยู่</p>							
+				<p class="grid_8">	
+	            <label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            	บ้านส่วนตัว
+	            </label>
+				<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	           		บ้านเช่า
+	            </label>
+	            <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	            	อาศัยบิดามารดา
+	            </label>
+				</p><br class="clear"/>
+	            
+	            <p class="grid_2">สถานะ</p>							
+				<p class="grid_8">	
+	            <label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            	โสด
+	            </label>
+				<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	           		สมรส
+	            </label>
+	            <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	            	หย่า
+	            </label>
+	             <label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	            	แยกกันอยู่
+	            </label>
+				</p><br class="clear"/>
+	             <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">Lorem Ipsum</p>
+	            <p class="grid_8"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	                     
+	            <p class="grid_2">ชื่อบิดา</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            <p class="grid_2 prefix_2">อายุ<input type="text" id="name" name ="name" class="round input-small" /></p>
+	            <p class="grid_2">อาชีพ<input type="text" id="name" name ="name" class="round input-small" /></p>
+	            <p class="grid_4">
+	            	<label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            	มีชีวิต
+	            </label>
+				<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	           		ถึงแก่กรรม
+	            </label>
+	            </p><br class="clear"/>
+	            
+	            <p class="grid_2">ชื่อมารดา</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            <p class="grid_2 prefix_2">อายุ<input type="text" id="name" name ="name" class="round input-small" /></p>
+	            <p class="grid_2">อาชีพ<input type="text" id="name" name ="name" class="round input-small" /></p>
+	            <p class="grid_4">
+	            	<label for="selected-radio" class="alt-label"><input type="radio" id="test" name="test" checked="checked" />
+	            	มีชีวิต
+	            </label>
+				<label for="unselected-radio" class="alt-label"><input type="radio" id="test" name="test" />
+	           		ถึงแก่กรรม
+	            </label>
+	            </p><br class="clear"/>
+	           
+	            
+	            <p class="grid_4">บุคคลอ้างอิงที่ติดต่อได้</p><br class="clear"/>
+	            
+	            <p class="grid_2">ชื่อ</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">ความสัมพันธ์</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">สถานที่ทำงาน</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">ตำแหน่ง</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	            
+	            <p class="grid_2">เบอร์โทรศัพท์</p>
+	            <p class="grid_2"><input type="text" id="name" name ="name" class="round" /></p><br class="clear"/>
+	                  
+	           	<p class="grid_12 center"><a href="#" class="save-button blue round">บันทึก</a></p>
+	            
+	      	</div>	
+	    </div><!--end content -->
 <?php
 			include ("include/footer.php");
 		} //end sql_user
