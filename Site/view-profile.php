2fa999c1ece831d495978979ce40d28c58814b31
nikom2532 <nikom2532@hotmail.com>
2013-11-01 01:13:12 +0700

    .

--- Site/view-profile.php
+++ Site/view-profile.php
@@ -38,106 +38,250 @@ else{
 						?>" width="144" height="143" />
 					</div>
 					<br class="clear"/>
-			
+					
+					
+					
+					
+					
+					
 					<p class="grid_2">
-						Lorem Ipsum
+						วันเกิด
 					</p>
 					<p class="grid_8">
-						Lorem Ipsum
+						<?php echo $rs_user["birthdate"]; ?>
 					</p>
 					<br class="clear"/>
-			
+					
+					<p class="grid_2">
+						สถาทที่เกิด
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["place_of_birth"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						อายุ
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["age"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						สัญชาติ
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["nationality"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						ศาสนา
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["religion"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						ส่วนสูง
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["height"]; ?> 
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						น้ำหนัก
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["weight"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						หมู่เลือด
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["blood"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						ตำหนิ
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["lesion"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						ที่อยู่ปัจจุบัน
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["current_address"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						เบอร์โทรศัพท์
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["phone_number"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						E-mail
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["email"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						ชื่อคู่สมรส
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["pouse_name"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						จำนวนบุตร
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["number_of_children"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						สถานะทางทหาร
+					</p>
+					<p class="grid_8 ">
+						<?php $rs_user["military_status"]; ?>
+					</p>
+					<br class="clear"/>
+					<p class="grid_2">
+						สถานะที่อยู่
+					</p>
+					<p class="grid_8">
+						<?php $rs_user["current_address_status"]; ?>
+					</p>
+					<br class="clear"/>
+		
+					<p class="grid_2">
+						สถานะ
+					</p>
+					<p class="grid_8">
+						<?php echo $rs_user["relationship_status"]; ?>
+					</p>
+					<br class="clear"/>
 					<p class="grid_2">
 						ชื่อบิดา
 					</p>
 					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
+						<?php echo $rs_user["father_name"]; ?>
 					</p>
 					<br class="clear"/>
-			
+					<p class="grid_2 prefix_2">
+						อายุ
+						<?php echo $rs_user["father_age"]; ?>
+					</p>
+					<p class="grid_2">
+						อาชีพ
+						<?php echo $rs_user["father_career"]; ?>
+					</p>
+					<p class="grid_4">
+						<label for="father_live_status1" class="alt-label">
+							<input type="radio" id="father_live_status1" name="father_live_status" value="live" <?php 
+								if($rs_user["father_live_status"]=="live"){
+									?>checked="checked"<?php
+								}
+							?> />
+							มีชีวิต </label>
+						<label for="father_live_status2" class="alt-label">
+							<input type="radio" id="father_live_status2" name="father_live_status" value="died" <?php 
+								if($rs_user["father_live_status"]=="died"){
+									?>checked="checked"<?php
+								}
+							?> />
+							ถึงแก่กรรม </label>
+					</p>
+					<br class="clear"/>
+		
 					<p class="grid_2">
 						ชื่อมารดา
 					</p>
 					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
+						<input type="text" id="mother_name" name ="mother_name" class="round" value="<?php echo $rs_user["mother_name"]; ?>" />
 					</p>
 					<br class="clear"/>
-			
-					<p class="grid_2">
-						บุคคลที่ติดต่อได้
-					</p>
-					<p class="grid_2">
-						Lorem Ipsum
-					</p>
-					<p class="grid_3">
-						Lorem Ipsum
-					</p>
-					<br class="clear"/>
-			
 					<p class="grid_2 prefix_2">
-						Lorem Ipsum
+						อายุ
+						<input type="text" id="mother_age" name ="mother_age" class="round input-small" onkeypress="validate_number(event);" value="<?php echo $rs_user["mother_age"]; ?>" />
 					</p>
 					<p class="grid_2">
-						Lorem Ipsum
+						อาชีพ
+						<input type="text" id="mother_career" name ="mother_career" class="round input-small" value="<?php echo $rs_user["mother_career"]; ?>" />
+					</p>
+					<p class="grid_4">
+						<label for="mother_live_status1" class="alt-label">
+							<input type="radio" id="mother_live_status1" name="mother_live_status" value="live" <?php 
+								if($rs_user["mother_live_status"]=="live"){
+									?>checked="checked"<?php
+								}
+							?> />
+							มีชีวิต </label>
+						<label for="mother_live_status2" class="alt-label">
+							<input type="radio" id="mother_live_status2" name="mother_live_status" value="died" <?php 
+								if($rs_user["mother_live_status"]=="died"){
+									?>checked="checked"<?php
+								}
+							?> />
+							ถึงแก่กรรม </label>
 					</p>
 					<br class="clear"/>
-			
-					<p class="grid_2 prefix_2">
-						Lorem Ipsum
+<?php
+					$rs_user_ref="
+						SELECT *
+						FROM  `buildthedot_thaijobhd_user_account_reference_contacts`
+						WHERE `user_account_id` = '".$_SESSION["userid"]."' ;
+					";
+					$result_user_ref = @mysql_query($rs_user_ref);
+					$rs_user_ref = @mysql_fetch_array($result_user_ref)
+?>
+					<p class="grid_4">
+						บุคคลอ้างอิงที่ติดต่อได้
+					</p>
+					<br class="clear"/>
+		
+					<p class="grid_2">
+						ชื่อ
 					</p>
 					<p class="grid_2">
-						Lorem Ipsum
+						<input type="text" id="ref_name" name ="ref_name" class="round" value="<?php echo $$rs_user_ref["name"]; ?>" />
 					</p>
 					<br class="clear"/>
-			
-					<p class="grid_2 prefix_2">
-						Lorem Ipsum
+		
+					<p class="grid_2">
+						ความสัมพันธ์
 					</p>
 					<p class="grid_2">
-						Lorem Ipsum
+						<input type="text" id="ref_relationship" name ="ref_relationship" class="round" value="<?php echo $$rs_user_ref["relationship"]; ?>" />
 					</p>
 					<br class="clear"/>
-			
-					<h2 id="sub-title">ประวัติการศึกษา<span class="right"><a href="<?php echo $rootpath; ?>add-education.php" class="button round black right">แก้ไข</a></span></h2>
+		
 					<p class="grid_2">
-						Lorem Ipsum
+						สถานที่ทำงาน
+					</p>
+					<p class="grid_2">
+						<input type="text" id="ref_workplace" name ="ref_workplace" class="round" value="<?php echo $$rs_user_ref["workplace"]; ?>" />
 					</p>
 					<br class="clear"/>
-
-					<div class="prefix_2">
-						<div id="head-table1" class="grid_9">
-							<p class="grid_2 center">
-								ระดับการศึกษา
-							</p>
-							<p class="grid_3 center">
-								สถาบัน
-							</p>
-							<p class="grid_1 center">
-								ปี
-							</p>
-							<p class="grid_2 center">
-								วุฒิการศึกษา
-							</p>
-						</div>
-					</div>
+		
+					<p class="grid_2">
+						ตำแหน่ง
+					</p>
+					<p class="grid_2">
+						<input type="text" id="ref_position" name ="ref_position" class="round" value="<?php echo $$rs_user_ref["position"]; ?>" />
+					</p>
+					<br class="clear"/>
+		
+					<p class="grid_2">
+						เบอร์โทรศัพท์
+					</p>
+					<p class="grid_2">
+						<input type="text" id="ref_phone_number" name ="ref_phone_number" class="round" value="<?php echo $$rs_user_ref["phone_number"]; ?>" />
+					</p>
 					<br class="clear"/>
 <?php
 					$sql_edu="
