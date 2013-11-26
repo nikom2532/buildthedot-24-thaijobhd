0e65719d88d8b9ecb74077339dd2fa28d94ab032
nikom2532 <arming@buildthedot.com>
2013-11-26 22:57:01 +0700

    .a

--- Site/admin/include/connect-to-database.php
+++ Site/admin/include/connect-to-database.php
@@ -1,13 +1,13 @@
 <?php
 //<<<<<<< HEAD
        //$cn = @mysql_connect("localhost","root","");
-	   $cn = @mysql_connect("localhost","root","");
+	   $cn = @mysql_connect("localhost","iming","iming");
 	   //mysql_select_db("buildthe_thaijobhd",$cn);
 	//   mysql_select_db("buildthe_ThaiJobHD",$cn);
 //=======
       // $cn = @mysql_connect("localhost","root","");
 	   //$cn = @mysql_connect("localhost","iming","iming");
-	   mysql_select_db("thai_jobhd_final",$cn);
+	   mysql_select_db("buildthedot_24thaijobhd",$cn);
 	   //mysql_select_db("buildthedot_24ThaiJobHD",$cn);
 //>>>>>>> 0c67dceb2e06ae724bec53a5e37405d86cce0e5f
 	   //mysql_query("SET NAMES tis620",$cn);
