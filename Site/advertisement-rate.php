92ab2ebf23646a4b5b29a4f42558533be0212e83
arming <nikom2532@gmail.com>
2014-02-13 23:01:02 +0700

    frontend: advertisement-rate

--- Site/advertisement-rate.php
+++ Site/advertisement-rate.php
@@ -14,19 +14,38 @@ include ("include/top-menu.php");
 		<div class="grid_5">
 			<h2 class="text-grey">ตำแหน่งโฆษณา</h2>
 			<div><img src="admin/images/bannerChart.png" /></div>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-			</p>
+			<p><?php
+				$sql="
+					SELECT `detail`
+					FROM  `buildthedot_thaijobhd_detail` 
+					WHERE `title` = 'advertisement-rate1'
+				;";
+				$result=@mysql_query($sql);
+				while($rs=@mysql_fetch_array($result)){
+					echo $rs["detail"];
+				}
+			?></p>
 		</div>
 
 		<div class="grid_6">
 			<h2 class="prefix_1 text-grey">ติดต่อโฆษณา</h2>
-			<p class="prefix_1">
-				โทร (622) 999 9999
-			</p>
-			<p class="prefix_1">
-				Email  a@b.com
-			</p>
+			<p class="prefix_1"><?php
+				//โทร (622) 999 9999
+			?></p>
+			<p class="prefix_1"><?php
+				//Email  a@b.com
+			?></p>
+			<p class="prefix_1"><?php
+				$sql="
+					SELECT `detail`
+					FROM  `buildthedot_thaijobhd_detail` 
+					WHERE `title` = 'advertisement-rate2'
+				;";
+				$result=@mysql_query($sql);
+				while($rs=@mysql_fetch_array($result)){
+					echo $rs["detail"];
+				}
+			?></p>
 		</div>
 
 	</div>
